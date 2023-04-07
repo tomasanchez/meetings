49ddcd98227546fd0a7f80a350b8e3513c8faec5
Leanz652 <lean.alvarez@live.com.ar>
2023-04-07 15:14:52 -0300

    TACS-1 Create Project Structure

---
+++ rest/src/main/java/com/grupo3/meetings/MeetingsApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.meetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsApplication.class, args);
+	}
+
+}
---
+++ rest/src/test/java/com/grupo3/meetings/MeetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.meetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
