4a85b3e639bef497c13420a9efc3d6115f27d544
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-30 17:57:03 -0300

    TACS-41 creates entrypoint package structure

---
+++ api-gateway/app/entrypoints/__init__.py
@@ -0,0 +1,5 @@
+"""
+Entrypoints are the places we drive our application from.
+In the official ports and adapters terminology, these are adapters too, and are referred to as primary, driving,
+or outward-facing adapters.
+"""
