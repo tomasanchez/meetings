47a61c2003545cb3e4ee947e4ec9157bb02daf71
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-05-01 13:36:17 -0300

    TACS-42 feat: add aiohttp adapter

--- api-gateway/tests/conftest.py
+++ api-gateway/tests/conftest.py
@@ -1,6 +1,12 @@
+"""
+This module contains pytest fixtures.
+"""
+
 import pytest
+import pytest_asyncio
 from starlette.testclient import TestClient
 
+from app.adapters.http_client import AiohttpClient, AsyncHttpClient
 from app.main import app
 
 
@@ -13,3 +19,17 @@ def fixture_test_client() -> TestClient:
         TestClient: A test client for the app.
     """
     return TestClient(app)
+
+
+@pytest_asyncio.fixture(name="aio_http_client")
+async def fixture_aio_http_client() -> AsyncHttpClient:
+    """
+    Create a test client for the FastAPI application.
+
+    Returns:
+        TestClient: A test client for the app.
+    """
+    client = AiohttpClient()
+    client.get_aiohttp_client()
+    yield client
+    await client.close_aiohttp_client()
