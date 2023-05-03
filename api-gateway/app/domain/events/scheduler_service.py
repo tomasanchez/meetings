9180c68f7aa96ff6d3b886f10fef1e46944f38af
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-05-02 23:59:45 -0300

    TACS-44 feat(events): add scheduler events

---
+++ api-gateway/app/domain/events/scheduler_service.py
@@ -0,0 +1,33 @@
+"""
+Events from scheduler service.
+"""
+import datetime
+
+from pydantic import Field
+
+from app.domain.schemas import CamelCaseModel
+
+
+class OptionVoted(CamelCaseModel):
+    """
+    Event emitted when an option is voted.
+    """
+    date: datetime.datetime = Field(description="A tentative date for a meeting", example="2021-01-01T09:30:00")
+    votes: list[str] = Field(description="A list of usernames that voted for the option.", example=["johndoe"])
+
+
+class MeetingScheduled(CamelCaseModel):
+    """
+    Event emitted when a meeting is scheduled.
+    """
+
+    id: str = Field(description="The meeting's unique identifier.", example="6442ee3291a1304d4c88ffc9")
+    organizer: str = Field(description="Username of who scheduled the meeting.", example="johndoe")
+    voting: bool = Field(description="Whether mode voting is enabled.", example=True)
+    title: str = Field(description="The meeting's title.", example="Sprint Planning")
+    description: str | None = Field(description="The meeting's description.", example="Planning the next sprint.")
+    location: str | None = Field(description="The meeting's location.", example="Room 1")
+    date: datetime.datetime | None = Field(description="The meeting's date.", example="2021-01-01T09:30:00")
+    guests: list[str] = Field(description="A list of guests.", example=["johndoe", "clarasmith"], default_factory=list)
+    options: list[OptionVoted] = Field(description="A list of options.",
+                                       example=["2021-01-01T09:30:00", "2021-01-01T10:30:00"])
