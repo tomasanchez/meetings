f88f610205e0fcee08c5e264ab36a3d9ee5fac09
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-05-25 09:50:36 -0300

    TACS-51 refactor: token authorization
    changes path to /me
    changes method to get
    adds authentication header validation

--- auth/src/auth/entrypoints/v1/auth.py
+++ auth/src/auth/entrypoints/v1/auth.py
@@ -1,11 +1,12 @@
 """Authentication & Authorization Entry Points
 
 """
+
 from fastapi import APIRouter, HTTPException
 from starlette.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 
-from auth.app.dependencies import AuthDependency
-from auth.domain.commands import AuthenticateUser, AuthorizeToken
+from auth.app.dependencies import AuthDependency, BearerTokenAuth
+from auth.domain.commands import AuthenticateUser
 from auth.domain.events import TokenGenerated, UserAuthenticated
 from auth.domain.schemas import ResponseModel
 from auth.service_layer.errors import InvalidCredentialsError
@@ -26,15 +27,16 @@ async def log_in(command: AuthenticateUser, auth_service: AuthDependency) -> Res
     return ResponseModel(data=TokenGenerated(token=token))
 
 
-@router.post('/user', status_code=HTTP_200_OK, tags=["Commands"])
-async def authorize_token(
-        command: AuthorizeToken,
-        auth_service: AuthDependency) -> ResponseModel[UserAuthenticated]:
+@router.get('/me', status_code=HTTP_200_OK, tags=["Commands"])
+async def me(
+        token: BearerTokenAuth,
+        auth_service: AuthDependency,
+) -> ResponseModel[UserAuthenticated]:
     """
     Authorizes a token, providing the user's information.
     """
     try:
-        user = auth_service.authorize(command.token)
+        user = auth_service.authorize(token.credentials)
     except InvalidCredentialsError as e:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail=str(e))
 
