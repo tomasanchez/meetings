04d22e2ef1ec39670af3d74c1d73b6562b4b072c
Leanz652 <lean.alvarez@live.com.ar>
2023-05-25 13:58:29 -0300

    TACS-54 Protected Component + Docker CFG

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,35 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorPage, LoginPage, HomePage } from "./pages";
 import { EventDetails } from "./pages/EventDetail/EventDetails";
+import { ProtectedRoute } from "./components/Guards/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
-    path: 'login',
-    element: <LoginPage/>
-  }
-  ,
+    path: "login",
+    element: <LoginPage />,
+  },
   {
-    path: '/', element: <HomePage/>,
+    path: "/",
+    element: <HomePage />,
     children: [
       {
         path: "/:id",
-        element: <EventDetails />,
+        element: (
+          <ProtectedRoute>
+            <EventDetails />
+          </ProtectedRoute>
+        ),
       },
     ],
-  }
-  ,
+  },
   {
-    path: '*', element: <ErrorPage/>
-  }
+    path: "*",
+    element: <ErrorPage />,
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router} ></RouterProvider>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
