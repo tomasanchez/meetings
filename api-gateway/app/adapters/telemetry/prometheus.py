ffab09f7b0d1a66e6818696ce3779196653eca0c
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-06-19 21:27:42 -0300

    feat(telemetry): add basic dashboard

---
+++ api-gateway/app/adapters/telemetry/prometheus.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+    OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from prometheus_client import Counter, Gauge, Histogram
+from prometheus_client import REGISTRY
+from prometheus_client.openmetrics.exposition import (CONTENT_TYPE_LATEST,
+                                                      generate_latest)
+from starlette.requests import Request
+from starlette.responses import Response
+
+########################################################################################################################
+# Prometheus metrics
+########################################################################################################################
+
+INFO = Gauge(
+    "fastapi_app_info", "Application information.", [
+        "app_name"]
+)
+
+REQUESTS = Counter(
+    "fastapi_requests_total", "Total count of requests by method and path.", [
+        "method", "path", "app_name"]
+)
+
+RESPONSES = Counter(
+    "fastapi_responses_total",
+    "Total count of responses by method, path and status codes.",
+    ["method", "path", "status_code", "app_name"],
+)
+
+REQUESTS_PROCESSING_TIME = Histogram(
+    "fastapi_requests_duration_seconds",
+    "Histogram of requests processing time by path (in seconds)",
+    ["method", "path", "app_name"],
+)
+
+EXCEPTIONS = Counter(
+    "fastapi_exceptions_total",
+    "Total count of exceptions raised by path and exception type",
+    ["method", "path", "exception_type", "app_name"],
+)
+
+REQUESTS_IN_PROGRESS = Gauge(
+    "fastapi_requests_in_progress",
+    "Gauge of requests by method and path currently being processed",
+    ["method", "path", "app_name"],
+)
+
+EVENTS_SCHEDULED = Counter(
+    "fastapi_events_scheduled_total",
+    "Total count of events scheduled",
+    ["app_name"],
+)
+
+
+def metrics(request: Request) -> Response:
+    return Response(generate_latest(REGISTRY), headers={"Content-Type": CONTENT_TYPE_LATEST})
+
+
+def setting_otlp(app: FastAPI, app_name: str, endpoint: str, log_correlation: bool = True) -> None:
+    """
+    Sets Open Telemetry Exporter.
+    """
+
+    # Setting OpenTelemetry
+    # set the service name to show in traces
+    resource = Resource.create(attributes={
+        "service.name": app_name,
+        "compose_service": app_name
+    })
+
+    # set the tracer provider
+    tracer = TracerProvider(resource=resource)
+    trace.set_tracer_provider(tracer)
+
+    tracer.add_span_processor(BatchSpanProcessor(
+        OTLPSpanExporter(endpoint=endpoint)))
+
+    if log_correlation:
+        LoggingInstrumentor().instrument(set_logging_format=True)
+
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
